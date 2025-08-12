c98cbf5e741e720fd1913f24e98e6ba4df51a7ea
sbx_user1051 <sbx_user1051@169.254.88.9>
2025-08-12 10:22:07 +0000

    Tue Aug 12 10:22:07 UTC 2025

--- src/test/kotlin/bdd/steps/SumSteps.kt
+++ src/test/kotlin/bdd/steps/SumSteps.kt
@@ -1,4 +1,8 @@
 package bdd.steps
 
 class SumSteps {
+    private var a = 0
+    private var b = 0
+    private var result = 0
+    private val solver = SumSolution()
 }
--- src/test/resources/features/sum.feature
+++ src/test/resources/features/sum.feature
@@ -1,1 +1,6 @@
 Feature: SUM R1 - add two integers
+
+Scenario: add 2 and 2
+Given two integers 2 and 2
+When I sum the numbers
+Then the result should be 4
