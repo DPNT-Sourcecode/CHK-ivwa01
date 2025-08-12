142f18d986207c8b0ef5189f02444fd126719326
sbx_user1051 <sbx_user1051@169.254.60.189>
2025-08-12 13:34:20 +0000

    Tue Aug 12 13:34:20 UTC 2025

--- src/test/kotlin/bdd/steps/HelloSteps.kt
+++ src/test/kotlin/bdd/steps/HelloSteps.kt
@@ -1,4 +1,14 @@
 package bdd.steps
 
+import io.cucumber.java.en.Given
+import solutions.HLO.HelloSolution
+
 class HelloSteps {
+    private lateinit var name: String
+    private lateinit var msg: String
+    private val hello = HelloSolution()
+
+    @Given("a friend named ")
+
+
 }
