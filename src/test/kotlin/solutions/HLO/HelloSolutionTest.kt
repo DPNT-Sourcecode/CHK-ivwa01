2fab38ca1a2147e2e251c7d61db3a7e06af18ac6
sbx_user1051 <sbx_user1051@169.254.60.189>
2025-08-12 16:23:35 +0000

    Tue Aug 12 16:23:35 UTC 2025

--- src/test/kotlin/solutions/HLO/HelloSolutionTest.kt
+++ src/test/kotlin/solutions/HLO/HelloSolutionTest.kt
@@ -5,8 +5,7 @@ import java.sql.DriverManager.println
 class HelloSolutionTest {
     @Test
     fun testHello() {
-        val hello = HelloSolution()
-        val result = hello.hello("Alice")
-        println(">>> HelloSolution says: '$result'")
+        val result = HelloSolution().hello("Alice")
+        println("Output from HelloSolution: $result")
     }
 }
