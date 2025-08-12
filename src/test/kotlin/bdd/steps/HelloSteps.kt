86ea02bdc61e5219e7a28b2dfa14b15bd7c78a42
sbx_user1051 <sbx_user1051@169.254.60.189>
2025-08-12 13:40:20 +0000

    Tue Aug 12 13:40:20 UTC 2025

--- src/test/kotlin/bdd/steps/HelloSteps.kt
+++ src/test/kotlin/bdd/steps/HelloSteps.kt
@@ -1,6 +1,9 @@
 package bdd.steps
 
 import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+import org.junit.jupiter.api.Assertions
 import solutions.HLO.HelloSolution
 
 class HelloSteps {
@@ -8,7 +11,13 @@ class HelloSteps {
     private lateinit var msg: String
     private val hello = HelloSolution()
 
-    @Given("a friend named {")
+    @Given("a friend named {word}")
+    fun aFiendName(n: String) {name = n}
 
+    @When("I say hello")
+    fun iSayHello() {msg = hello.hello(name)}
+
+    @Then("the message should be {word}")
+    fun theMessageShouldBe(expected: String) { Assertions.assertEquals(expected, msg)}
 
 }
