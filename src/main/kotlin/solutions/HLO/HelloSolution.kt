f2921fccc5ccf4d69472dec0a6cacd22a35a95aa
sbx_user1051 <sbx_user1051@169.254.60.189>
2025-08-12 14:55:20 +0000

    Tue Aug 12 14:55:20 UTC 2025

--- src/main/kotlin/solutions/HLO/HelloSolution.kt
+++ src/main/kotlin/solutions/HLO/HelloSolution.kt
@@ -2,9 +2,9 @@ package solutions.HLO
 
 class HelloSolution {
     fun hello(friendName: String): String {
-        if (friendName.length == 0 || friendName == "\"\""){
+        if (friendName.length == 0 || friendName == "\"\"") {
             return "Hello, World!"
         }
-        return "Hello," + friendName + "!"
+        return "Hello, " + friendName + "!"
     }
 }
