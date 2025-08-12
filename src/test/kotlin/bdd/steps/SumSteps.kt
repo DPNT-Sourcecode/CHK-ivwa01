f646892a6a097831130f8ecb36c7035cde757793
sbx_user1051 <sbx_user1051@169.254.88.9>
2025-08-12 10:25:06 +0000

    Tue Aug 12 10:25:06 UTC 2025

--- src/test/kotlin/bdd/steps/SumSteps.kt
+++ src/test/kotlin/bdd/steps/SumSteps.kt
@@ -1,8 +1,22 @@
 package bdd.steps
+import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+import org.junit.jupiter.api.Assertions.assertEquals
+import solutions.SUM.SumSolution
 
 class SumSteps {
     private var a = 0
     private var b = 0
     private var result = 0
     private val solver = SumSolution()
+
+    @Given("two integers {int} and {int}")
+    fun twoIntegers(x: Int, y: Int) { a = x; b = y }
+
+    @When("I sum the numbers")
+    fun sumNumbers() { result = solver.sum(a, b) }
+
+    @Then("the result should be {int}")
+    fun theResultShouldBe(expected: Int) { assertEquals(expected, result) }
 }
