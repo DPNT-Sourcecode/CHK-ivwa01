60db4d44405c453332e7a6523d6acd71b5ab4514
sbx_user1051 <sbx_user1051@169.254.60.189>
2025-08-12 13:52:20 +0000

    Tue Aug 12 13:52:20 UTC 2025

--- src/test/kotlin/bdd/RunCucumberTest.kt
+++ src/test/kotlin/bdd/RunCucumberTest.kt
@@ -1,4 +1,6 @@
 package bdd
+import io.cucumber.junit.platform.engine.Constants
+import org.junit.platform.suite.api.ConfigurationParameter
 import org.junit.platform.suite.api.IncludeEngines
 import org.junit.platform.suite.api.SelectClasspathResource
 import org.junit.platform.suite.api.Suite
@@ -6,4 +8,6 @@ import org.junit.platform.suite.api.Suite
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "bdd.steps")
 class RunCucumberTest
+
