850d51d050b36b40e669db731e27fc053806cbe8
sbx_user1051 <sbx_user1051@169.254.18.171>
2025-08-14 09:39:53 +0000

    Thu Aug 14 09:39:53 UTC 2025

--- src/test/kotlin/bdd/steps/CheckoutSteps.kt
+++ src/test/kotlin/bdd/steps/CheckoutSteps.kt
@@ -1,9 +1,26 @@
 import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+import org.junit.jupiter.api.Assertions.assertEquals
+import solutions.CHK.CheckoutSolution
 
 class CheckoutSteps {
+    private var skus: String? = null
+    private var result: Int = 0
+    private val checkout = CheckoutSolution()
 
     @Given("a basket with items {string}")
     fun basketWithItems(skus: String?){
-        
+        this.skus = skus
+    }
+
+    @When("calculate total")
+    fun calculateTotal(){
+        result = checkout.checkout(skus)
+    }
+
+    @Then("the total should be {int}")
+    fun totalShouldBe(expected: Int){
+        assertEquals(expected, result)
     }
 }
