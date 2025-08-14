6795d6bf72d6e06b9ed725ec42b5e41481e46857
sbx_user1051 <sbx_user1051@169.254.18.171>
2025-08-14 10:33:54 +0000

    Thu Aug 14 10:33:54 UTC 2025

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -2,6 +2,12 @@ package solutions.CHK
 
 class CheckoutSolution {
     fun checkout(skus: String?): Int {
+        if(skus.isNullOrEmpty())
         return -1
+
+        var total = 0
+
+        
+        return total
     }
 }
