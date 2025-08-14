734aab80f0bd145445d6e3e5262f538a8faa929a
sbx_user1051 <sbx_user1051@169.254.18.171>
2025-08-14 15:40:23 +0000

    Thu Aug 14 15:40:23 UTC 2025

--- src/main/kotlin/solutions/CHK/CheckoutSolution.kt
+++ src/main/kotlin/solutions/CHK/CheckoutSolution.kt
@@ -1,14 +1,12 @@
 package solutions.CHK
 
 class CheckoutSolution {
-    fun checkout(skus: String?): Int {
+    fun checkout(skus: String?, isR2: Boolean = true): Int {
 
-        if (skus == null) return -1
+        if (skus == null || skus.isEmpty()) return -1
 
-        if(skus.isEmpty()) return 0
-
-        if(skus.any { it !in "ABCDE"}) return -1
-
+        if (skus.any { it !in if (isR2) "ABCDE" else "ABCD" }) return -1
+        
         var total = 0
         val counts = skus.groupingBy { it }.eachCount()
 
@@ -18,14 +16,20 @@ class CheckoutSolution {
         val countD = counts.getOrDefault('D',0)
         val countE = counts.getOrDefault('E',0)
 
-        val freeBSFromE = countE / 2
+        val freeBSFromE = if (isR2) countE / 2 else 0
         val effectiveCountB = maxOf(0, countB - freeBSFromE)
 
-        total += (countA / 5) * 200 + ((countA % 5) / 3) * 130 + ((countA % 5) % 3) * 50
+        total += if (isR2) {
+            (countA / 5) * 200 + ((countA % 5) / 3) * 130 + ((countA % 5) % 3) * 50
+        } else {
+            (countA / 3) * 130 + (countA % 3) * 50
+        }
+
         total += (effectiveCountB / 2) * 45 + (effectiveCountB % 2) * 30
         total += countC * 20
         total += countD * 15
-        total += countE * 40
+
+        if (isR2) total += countE * 40
 
         return total
     }
